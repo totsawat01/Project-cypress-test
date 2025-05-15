45f208672831bdb6c7756d2fd647149d8560302a
totsawat <jjkid83@gmail.com>
2025-05-15 14:13:18 +0700

    update script ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,27 +1,13 @@
 import { defineConfig } from "cypress";
-import { Client } from "pg";
 
 export default defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on('task', {
-        async queryDb(query: string) {
-          const client = new Client({
-            user: "postgres",
-            host: "localhost",
-            database: "testdb",
-            password: "password123",
-            port: 5432,
-          });
-
-          await client.connect();
-          const result = await client.query(query);
-          await client.end();
-          return result.rows;
-        },
-      });
-    },
-    specPattern: "cypress/tcs/**/*.cy.{js,ts,jsx,tsx}"
   }
 });
