e25e842235b630dc4a0213c8c63925cd4a057055
totsawat <jjkid83@gmail.com>
2025-05-15 14:22:34 +0700

    update script ci

--- cypress/tcs/testCase.cy.ts
+++ cypress/tcs/testCase.cy.ts
@@ -1,4 +1,4 @@
-describe('API Test (Docker)', () => {
+describe('API Test connect (Docker)', () => {
     it('should respond with message', () => {
         cy.request('http://localhost:3000/hello').then((res) => {
             expect(res.status).to.eq(200);
